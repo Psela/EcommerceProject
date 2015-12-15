2556fd8d87c7cf6059e708a47527ba623a178cd4
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-15 14:36:49 +0000

    GetAllCustomers() added + test

--- EcommerceProject/EntityFramework/Select/FindCustomer.cs
+++ EcommerceProject/EntityFramework/Select/FindCustomer.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace EcommerceProject.DatabaseModel.Select
 {
-    class FindCustomer
+    public class FindCustomer
     {
+        private ECommerceEntities context;
+
+        public FindCustomer(ECommerceEntities eCommerceEntities)
+        {
+            context = eCommerceEntities;
+        }
+
+        public List<CustomerData> GetAllCustomers()
+        {
+            using (context)
+            {
+                List<CustomerData> customers = context.CustomerDatas.ToList<CustomerData>();
+                return customers;  
+            }
+        }
     }
 }
