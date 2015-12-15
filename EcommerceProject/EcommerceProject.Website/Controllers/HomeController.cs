2798e413367139c63ead1978d696c6cdbf9cd48a
Francelp <francel_piedra@hotmail.com>
2015-12-15 14:15:34 +0000

    Layoutpage CSS

--- EcommerceProject/EcommerceProject.Website/Controllers/HomeController.cs
+++ EcommerceProject/EcommerceProject.Website/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ namespace EcommerceProject.Website.Controllers
   {
     DatabaseReader reader;
 
+    public HomeController()
+    {
+
+    }
+
     public HomeController(DatabaseReader DbReader)
     {
       reader = DbReader;
