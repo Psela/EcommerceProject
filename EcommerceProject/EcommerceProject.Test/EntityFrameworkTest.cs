365e813cdc1754f6aa4e10a09b5de76fad3be46e
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-15 12:52:54 +0000

    Added GetAllProducts() to Select folder with test

---
+++ EcommerceProject/EcommerceProject.Test/EntityFrameworkTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using EcommerceProject.DatabaseModel;
+using EcommerceProject.DatabaseModel.Select;
+
+
+namespace EcommerceProject.Test
+{
+
+    [TestClass]
+    public class EntityFrameworkTest
+    {
+        [TestMethod]
+        public void test_thatGetAllProducts_returnsAListOfProductData_whenCalled()
+        {
+            //Arrange
+            Mock<ECommerceEntities> context = new Mock<ECommerceEntities>();
+            List<ProductData> productDataList = new List<ProductData>();
+            Mock<ProductData> mockProductData = new Mock<ProductData>();
+            mockProductData.SetupAllProperties();
+
+            productDataList.Add(mockProductData.Object);
+
+            DbSet<ProductData> mockedDataSet = GetQueryableMockSet.GetQueryableMockDbSet<ProductData>(productDataList);
+            context.SetupAllProperties();
+            context.Object.ProductDatas = mockedDataSet;
+            
+            FindProduct findProduct = new FindProduct(context.Object);
+
+            //Act
+            List<ProductData> list = findProduct.GetAllProducts();
+
+            //Assert
+            CollectionAssert.AreEqual(productDataList, list);
+        }
+    }
+}
--- EcommerceProject/EntityFramework/Select/FindOrder.cs
+++ EcommerceProject/EntityFramework/Select/FindOrder.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityFramework.Select
+namespace EcommerceProject.DatabaseModel.Select
 {
     class FindOrder
     {
