eb01eb5db211cecf45cf157bd3a11fc79ac06791
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-17 16:51:26 +0000

    added the updateproductviewmodel

---
+++ EcommerceProject/EcommerceProject.AdminPortal/UpdateProductViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceProject.AdminPortal
+{
+    public class UpdateProductViewModel:INotifyPropertyChanged
+    {
+        private string _source;
+        public string source 
+        {
+            get { return _source; }
+            set 
+            {
+                _source = value;
+                onPropertyChanged("source");
+            }
+        }
+
+        public UpdateProductViewModel()
+        {
+            source = "UpdateProductView.xaml";
+        }
+
+        private void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
