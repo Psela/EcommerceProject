b22b991722bc8b29b06ca8992cc5d01a1e705a1a
teejayt <tolanijayt4ril@yahoo.com>
2016-01-04 10:37:20 +0000

    Changed the reference in the test for the findproductviewmodel. The HomeView  and viewmodel was added to the HomeVM folder

--- EcommerceProject/EcommerceProject.AdminPortal/HomeVM/HomeView.xaml.cs
+++ EcommerceProject/EcommerceProject.AdminPortal/HomeVM/HomeView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EcommerceProject.AdminPortal
+namespace EcommerceProject.AdminPortal.HomeVM
 {
     /// <summary>
     /// Interaction logic for HomeView.xaml
